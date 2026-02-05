b11855d86e0686f59fdb69492071da1eabea849a
xushuhui <xushuhui@fm-vip.com>
2026-02-05 11:05:45 +0800

    feat(context): add Unwrap method to expose ResponseWriter
    Add Unwrap method to allow accessing the underlying http.ResponseWriter for more direct control when needed

--- context.go
+++ context.go
@@ -323,7 +323,7 @@ func (c *Context) SetError(err error) {
 
 // Flush sends any buffered content to the client
 // Returns false if the underlying ResponseWriter doesn't support flushing
-func (c *Context) Flush()  {
+func (c *Context) Flush() {
 	if flusher, ok := c.ResponseWriter.(http.Flusher); ok {
 		flusher.Flush()
 	}
@@ -332,4 +332,8 @@ func (c *Context) Flush()  {
 // Error returns the error stored in the context
 func (c *Context) Error() error {
 	return c.err
+}
+// Unwrap returns the underlying http.ResponseWriter
+func (c *Context) Unwrap() http.ResponseWriter {
+	return c.ResponseWriter
 }
